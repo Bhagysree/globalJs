af5a25110d2e4182d2a3c059f3b8c8215187cd03
Bhagyasree <tellibhagyasree17@gmail.com>
2025-08-13 14:26:12 +0000

    login page

---
+++ task/one.js
@@ -0,0 +1,6 @@
+import readline from 'readline-sync';
+
+let age = readline.question('What is your age? ');
+    console.log(age);
+let name = readline.question('What is your name? ');
+    console.log(name);
